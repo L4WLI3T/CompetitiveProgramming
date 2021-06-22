65ff474c456a0da245e30b0e98b7449ba67e428f
L4WLI3T <48093400+L4WLI3T@users.noreply.github.com>
2021-06-22 18:20:35 +0530

    Create HOOPS.cpp

---
+++ Codechef/2021/May/LTIME96C/HOOPS.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include<bits/stdc++.h>
+#define ll long long
+int main() {
+	// your code goes here
+	ll t; cin>>t;
+	while(t--)
+	{
+	    ll n;
+	    cin>>n;
+	    if(n%2==1)
+	    {
+	        cout<<ceil(n/2)+1<<"\n";
+	    }
+	    else
+	    {
+	       
+	    }
+	}
+	return 0;
+}
