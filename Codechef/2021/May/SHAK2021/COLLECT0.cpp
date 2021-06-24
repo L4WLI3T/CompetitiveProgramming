c4a4e4dc49fdb09824a45a98d054193f2616d129
L4WLI3T <48093400+L4WLI3T@users.noreply.github.com>
2021-06-24 12:43:00 +0530

    Create COLLECT0.cpp

---
+++ Codechef/2021/May/SHAK2021/COLLECT0.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a>b){cout<<"A"<<"\n";}
+        else {cout<<"B"<<"\n";}
+    }
+	return 0;
+}
