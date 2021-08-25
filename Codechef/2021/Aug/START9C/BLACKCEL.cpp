f66b2aba00cb98a525affd1b9569cb3f47f70ccb
L4WLI3T <48093400+L4WLI3T@users.noreply.github.com>
2021-08-25 23:39:55 +0530

    Create BLACKCEL.cpp

---
+++ Codechef/2021/Aug/START9C/BLACKCEL.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+#define ll long long
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1; 
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<n/2*n<<"\n";
+    }
+	return 0;
+}
