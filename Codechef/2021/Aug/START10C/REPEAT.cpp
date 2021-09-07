7cedb335d7c5229b0c88cf5e3c0d0620ba78c98d
L4WLI3T <48093400+L4WLI3T@users.noreply.github.com>
2021-09-07 19:15:12 +0530

    Create REPEAT.cpp

---
+++ Codechef/2021/Aug/START10C/REPEAT.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+#define ll long long
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;  cin>>t;
+    while(t--)
+    {
+        ll n,m,q,i,t2,chk=0,ct=1,k,s,sm=0,dif=0;
+        char t1;
+        cin>>n>>k>>s;
+        vector<pair<char,int>> v;
+        vector<int> vi;
+        set<int> st;
+        cout<<(s-(n*n))/(k-1)<<"\n";
+    }
+	return 0;
+}
